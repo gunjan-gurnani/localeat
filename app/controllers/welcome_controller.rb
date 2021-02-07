b0ae34beecdf8d63ed83758cb37099e104a3a689
Frederic Wong <frederic.wong255@gmail.com>
2021-02-07 17:34:20 +0000

    use partials
    Signed-off-by: Frederic Wong <frederic.wong255@gmail.com>

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,7 @@ class WelcomeController < ApplicationController
   def index
     @search = Business.ransack(params[:q])
     return unless params[:q].present? && params[:q].values.reject(&:blank?).any?
-      @businesses = @search.result(distinct: true).includes(:location)
+
+    @businesses = @search.result(distinct: true).includes(:location)
   end
 end

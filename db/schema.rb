932d3c7b659ae665f7bcbd85a3d1a9fc32d0b0ea
Gunjan Gurnani <gagurnani@gmail.com>
2021-02-13 15:35:17 +0000

    rebase and fix rubocop yml

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_163028) do
+ActiveRecord::Schema.define(version: 2021_02_09_212144) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
